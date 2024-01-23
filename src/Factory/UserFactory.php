1de5ac03c051b104b874814848ff7a8cbe37e986
Kuca Brice <brice.kuca@etudiant.univ-reims.fr>
2024-01-23 13:28:15 +0100

    feat: create user-factory with faker

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<User>
+ *
+ * @method        User|Proxy                     create(array|callable $attributes = [])
+ * @method static User|Proxy                     createOne(array $attributes = [])
+ * @method static User|Proxy                     find(object|array|mixed $criteria)
+ * @method static User|Proxy                     findOrCreate(array $attributes)
+ * @method static User|Proxy                     first(string $sortedField = 'id')
+ * @method static User|Proxy                     last(string $sortedField = 'id')
+ * @method static User|Proxy                     random(array $attributes = [])
+ * @method static User|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static UserRepository|RepositoryProxy repository()
+ * @method static User[]|Proxy[]                 all()
+ * @method static User[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static User[]|Proxy[]                 createSequence(array|callable $sequence)
+ * @method static User[]|Proxy[]                 findBy(array $attributes)
+ * @method static User[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static User[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ */
+final class UserFactory extends ModelFactory
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected static function createAvatar(string $value)
+    {
+        $icon = new \Jdenticon\Identicon();
+        $icon->setValue($value);
+        $icon->setSize(50);
+
+        return fopen($icon->getImageDataUri('png'), 'r');
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        $firstname = self::faker()->firstName();
+        $lastname = self::faker()->lastName();
+
+        return [
+            'login' => self::faker()->unique()->numerify('user-###'),
+            'roles' => [],
+            'password' => 'test',
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'mail' => self::faker()->unique()->safeEmail(),
+        ];
+    }
+}
